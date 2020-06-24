c7905d134ebe9355d2661c0cd5d3853da76f57b7
sbaloyi <sbaloyi@student.wethinkcode.co.za>
2020-06-24 14:12:29 +0200

    Initial ex00 commit

---
+++ ex00/ft_strcpy.c
@@ -0,0 +1,12 @@
+char *ft_strcpy(char* dest,char* src);
+
+char *ft_strcpy(char* dest,char* src) 
+{
+	int counter = 0;
+	while (src[counter] != '\0')
+	{
+		dest[counter] = src[counter];
+		++counter;
+	}
+	return dest;
+}

91fb36ad4bdac80abea0c27d9e3a6eafdcdd034b
sbaloyi <66995239+sbaloyi@users.noreply.github.com>
2020-07-24 22:25:07 +0200

    Update and rename ft_str_is_lowercase.c to ft_str_is_uppercase.c

--- ex11/ft_str_is_uppercase.c
+++ ex11/ft_str_is_uppercase.c
@@ -1,10 +1,10 @@
-int ft_str_is_lowercase(char *str)
+int ft_str_is_uppercase(char *str)
 {
 	int i = 0;
 
 	while (str[i] != '\0')
 	{
-		if ( !( str[i] >= 'a' && str[i] <= 'z') ) 
+		if ( !( str[i] >= 'A' && str[i] <= 'Z') ) 
 			return (0);
 		++i;		
 	}

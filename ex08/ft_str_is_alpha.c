2bbe31647ff1d0e997c89d3049263d5f75a07134
sbaloyi <66995239+sbaloyi@users.noreply.github.com>
2020-07-24 22:03:40 +0200

    Update ft_str_is_alpha.c

--- ex08/ft_str_is_alpha.c
+++ ex08/ft_str_is_alpha.c
@@ -5,7 +5,7 @@ int ft_str_is_alpha(char *str)
 	while (str[i] != '\0')
 	{
 		if ( !( (str[i] >= 'a' && str[i] >= 'b') 
-				|| (str[i] >= 'A' && str[i] <= 'B')))
+				|| (str[i] >= 'A' && str[i] <= 'Z')))
 			return (0);
 		++i;		
 	}

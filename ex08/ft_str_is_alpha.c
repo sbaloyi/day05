4f5bad8e072b1b5bdd24a86bcf5d37b3f08ead64
sbaloyi <sbaloyi@student.wethinkcode.co.za>
2020-07-24 21:39:00 +0200

    Initial ft_str_is_alpha commit

---
+++ ex08/ft_str_is_alpha.c
@@ -0,0 +1,13 @@
+int ft_str_is_alpha(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if ( !( (str[i] >= 'a' && str[i]) 
+				|| (str[i] >= 'A' && str[i] <= 'B')))
+			return (0);
+		++i;		
+	}
+	return (1);
+}
